ce2c1b30ea1192bb0307ffbacb6ff53afd87f1de
ElizPN <pankevych.liza@gmail.com>
2022-11-01 14:37:26 +0100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
 // import "./App.css";
 import { useState } from "react";
+import CheckboxList from "./CkeckboxList";
 
 function App() {
-  const [toDolist, setTodolist] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState("");
-
-  function addItemToList(event: any) {
-    const newToDoList = toDolist;
-    newToDoList.push(inputValue);
-    setTodolist([...newToDoList]);
-    console.log(newToDoList);
-  }
-
-  return (
-    <div>
-      <input
-        value={inputValue}
-        onChange={(event) => setInputValue(event.target.value)}
-      />
-      <button onClick={addItemToList}> submit</button>
-      <ul>
-        {toDolist.map((item, index) => {
-          return <li key={index}>{item}</li>;
-        })}
-      </ul>
-    </div>
-  );
+  return <CheckboxList />;
 }
 
 export default App;
