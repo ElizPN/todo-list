4e29fb0644b6438765b15dba7fe232e2eafd1f9f
ElizPN <pankevych.liza@gmail.com>
2022-11-15 13:25:48 +0100

    wrap CkeckboxList to ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 // import "./App.css";
+import { ThemeProvider } from "@mui/system";
 import { useState } from "react";
 import CheckboxList from "./CkeckboxList";
+import { theme } from "./Theme";
 
 function App() {
-  return <CheckboxList />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CheckboxList />
+    </ThemeProvider>
+  );
 }
 
 export default App;
