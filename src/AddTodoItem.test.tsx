8e42501b75de78e223e7c2f0b3d457ffde80c7d5
ElizPN <pankevych.liza@gmail.com>
2022-11-28 14:03:35 +0100

    add three tests

--- src/AddTodoItem.test.tsx
+++ src/AddTodoItem.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import exp from "constants";
 import { ChangeEvent, MouseEvent } from "react";
 import { AddTodoItem } from "./AddTodoItem";
@@ -34,5 +34,84 @@ describe("AddTodoItem", () => {
 
     const buttonElement = screen.getByTestId("add-button");
     expect(buttonElement).toBeInTheDocument();
+  });
+
+  test("should call addItemTolist when inputValue has text ", () => {
+    // Arrange
+    const handeOnChange = jest.fn();
+    const addItemToList = jest.fn();
+    render(
+      <AddTodoItem
+        isDirty={false}
+        handeOnChange={handeOnChange}
+        inputValue={"some text"}
+        addItemToList={addItemToList}
+      />
+    );
+    const addButton = screen.getByTestId("add-button");
+    //Act
+    // simulate click on Addbutton
+    fireEvent.click(addButton);
+
+    //Assert
+    // addItemTolist shoud be called
+    expect(addItemToList).toHaveBeenCalled();
+  });
+
+  test("AddButton should be disabled when inputValue is empty ", () => {
+    // Arrange
+    const handeOnChange = jest.fn();
+    const addItemToList = jest.fn();
+    //Act
+    render(
+      <AddTodoItem
+        isDirty={false}
+        handeOnChange={handeOnChange}
+        inputValue={""}
+        addItemToList={addItemToList}
+      />
+    );
+    const addButton = screen.getByTestId("add-button");
+
+    //Assert
+    expect(addButton).toBeDisabled();
+  });
+
+  test("AddButton should be disabled when inputValue is empty ", () => {
+    // Arrange
+    const handeOnChange = jest.fn();
+    const addItemToList = jest.fn();
+    //Act
+    render(
+      <AddTodoItem
+        isDirty={false}
+        handeOnChange={handeOnChange}
+        inputValue={""}
+        addItemToList={addItemToList}
+      />
+    );
+    const addButton = screen.getByTestId("add-button");
+
+    //Assert
+    expect(addButton).toBeDisabled();
+  });
+
+  test("AddButton should be anabled when inputValue is not empty ", () => {
+    // Arrange
+    const handeOnChange = jest.fn();
+    const addItemToList = jest.fn();
+    //Act
+    render(
+      <AddTodoItem
+        isDirty={false}
+        handeOnChange={handeOnChange}
+        inputValue={"some text"}
+        addItemToList={addItemToList}
+      />
+    );
+    const addButton = screen.getByTestId("add-button");
+
+    //Assert
+    expect(addButton.getAttribute("disabled")).toBeNull();
   });
 });
