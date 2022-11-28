55213f27b2f72a5d286ef2367605f84655c4c85a
ElizPN <pankevych.liza@gmail.com>
2022-11-28 09:20:15 +0100

    wrap tests to "describe"

--- src/AddTodoItem.test.tsx
+++ src/AddTodoItem.test.tsx
@@ -3,34 +3,36 @@ import exp from "constants";
 import { ChangeEvent, MouseEvent } from "react";
 import { AddTodoItem } from "./AddTodoItem";
 
-test("render Let`s go text in input", () => {
-  const handeOnChange = jest.fn();
-  const addItemToList = jest.fn();
-  render(
-    <AddTodoItem
-      isDirty={false}
-      handeOnChange={handeOnChange}
-      inputValue={""}
-      addItemToList={addItemToList}
-    />
-  );
+describe("AddTodoItem", () => {
+  test("render Let`s go text in input", () => {
+    const handeOnChange = jest.fn();
+    const addItemToList = jest.fn();
+    render(
+      <AddTodoItem
+        isDirty={false}
+        handeOnChange={handeOnChange}
+        inputValue={""}
+        addItemToList={addItemToList}
+      />
+    );
 
-  const textElement = screen.getByTestId("add-item-textfield");
-  expect(textElement).toBeInTheDocument();
-});
+    const textElement = screen.getByTestId("add-item-textfield");
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("existing Add button ", () => {
-  const handeOnChange = jest.fn();
-  const addItemToList = jest.fn();
-  render(
-    <AddTodoItem
-      isDirty={false}
-      handeOnChange={handeOnChange}
-      inputValue={""}
-      addItemToList={addItemToList}
-    />
-  );
+  test("existing Add button ", () => {
+    const handeOnChange = jest.fn();
+    const addItemToList = jest.fn();
+    render(
+      <AddTodoItem
+        isDirty={false}
+        handeOnChange={handeOnChange}
+        inputValue={""}
+        addItemToList={addItemToList}
+      />
+    );
 
-  const buttonElement = screen.getByTestId("add-button");
-  expect(buttonElement).toBeInTheDocument();
+    const buttonElement = screen.getByTestId("add-button");
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
