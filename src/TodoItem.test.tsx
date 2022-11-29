7333d85abb566af65ce63f15515f21fd53c29193
ElizPN <pankevych.liza@gmail.com>
2022-11-29 13:48:45 +0100

    test handlerSaveItemText and handleToggleCheked

--- src/TodoItem.test.tsx
+++ src/TodoItem.test.tsx
@@ -67,68 +67,66 @@ describe("TodoItem", () => {
     expect(setInputEditItem).toBeCalledWith("some text");
   });
 
-   test("should call handlerSaveItem after click on SaveIcon", () => {
-     const setEditingIndex = jest.fn();
-     const setInputEditItem = jest.fn();
-     const handleDelteItemFromState = jest.fn();
-     const handleToggleCheked = jest.fn();
-     const handlerSaveItem = jest.fn();
+  test("should call handlerSaveItemText after click on SaveIcon", () => {
+    const setEditingIndex = jest.fn();
+    const setInputEditItem = jest.fn();
+    const handleDelteItemFromState = jest.fn();
+    const handleToggleCheked = jest.fn();
+    const handlerSaveItem = jest.fn();
 
-     const item = {
-       text: "some text",
-       checked: false,
-     };
-     render(
-       <TodoItem
-         editingIndex={1}
-         setEditingIndex={setEditingIndex}
-         inputEditItem={"some text"}
-         setInputEditItem={setInputEditItem}
-         handleDelteItemFromState={handleDelteItemFromState}
-         handleToggleCheked={handleToggleCheked}
-         handlerSaveItem={handlerSaveItem}
-         item={item}
-         index={1}
-         key={1}
-       />
-     );
+    const item = {
+      text: "some text",
+      checked: false,
+    };
+    render(
+      <TodoItem
+        editingIndex={1}
+        setEditingIndex={setEditingIndex}
+        inputEditItem={"some text"}
+        setInputEditItem={setInputEditItem}
+        handleDelteItemFromState={handleDelteItemFromState}
+        handleToggleCheked={handleToggleCheked}
+        handlerSaveItem={handlerSaveItem}
+        item={item}
+        index={1}
+        key={1}
+      />
+    );
 
-     const saveButton = screen.getByTestId("save-button");
-     fireEvent.click(saveButton);
-     expect(handlerSaveItem).toBeCalled()
-     
-   });
+    const saveButton = screen.getByTestId("save-button");
+    fireEvent.click(saveButton);
+    expect(handlerSaveItem).toBeCalledWith(1);
+    expect(setEditingIndex).toBeCalledWith(null);
+  });
 
-     test("should call handleToggleCheked after click on Checkbox", () => {
-       const setEditingIndex = jest.fn();
-       const setInputEditItem = jest.fn();
-       const handleDelteItemFromState = jest.fn();
-       const handleToggleCheked = jest.fn();
-       const handlerSaveItem = jest.fn();
+  test("should call handleToggleCheked after click on Checkbox", () => {
+    const setEditingIndex = jest.fn();
+    const setInputEditItem = jest.fn();
+    const handleDelteItemFromState = jest.fn();
+    const handleToggleCheked = jest.fn();
+    const handlerSaveItem = jest.fn();
 
-       const item = {
-         text: "some text",
-         checked: false,
-       };
-       render(
-         <TodoItem
-           editingIndex={1}
-           setEditingIndex={setEditingIndex}
-           inputEditItem={"some text"}
-           setInputEditItem={setInputEditItem}
-           handleDelteItemFromState={handleDelteItemFromState}
-           handleToggleCheked={handleToggleCheked}
-           handlerSaveItem={handlerSaveItem}
-           item={item}
-           index={2}
-           key={1}
-         />
-       );
+    const item = {
+      text: "some text",
+      checked: false,
+    };
+    render(
+      <TodoItem
+        editingIndex={1}
+        setEditingIndex={setEditingIndex}
+        inputEditItem={"some text"}
+        setInputEditItem={setInputEditItem}
+        handleDelteItemFromState={handleDelteItemFromState}
+        handleToggleCheked={handleToggleCheked}
+        handlerSaveItem={handlerSaveItem}
+        item={item}
+        index={2}
+        key={1}
+      />
+    );
 
-       const ckeckBox = screen.getByTestId("checkbox");
-       fireEvent.click(ckeckBox);
-       expect(handleToggleCheked).toBeCalled();
-     });
-
-  
+    const ckeckBox = screen.getByTestId("checkbox");
+    fireEvent.click(ckeckBox);
+    expect(handleToggleCheked).toBeCalled();
+  });
 });
